8bd51ae9d1a893acb80c8fd2d4fe958979d921dc
Komal Ghanwat <kggk9050@gmail.com>
2021-10-04 17:55:13 +0530

    [KOmal] added all files 04-10

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,23 +1,29 @@
 <template>
-    <header class="gallery_header">
-        <div class="left_header">
+    <nav class="navbar">
+        <div class="nav_header">
+        <div class="left_nav">
             <img src="../../assets/logod.png" alt="" srcset="">
-            <!-- <span class="discovery_header_text1">Discovery</span>
-            <span class="discovery_header_text2">Land Company</span> -->
-
         </div>
-        
-        <nav class="nav_header">
-            <ul class="nav_ul">
-                <li class="nav_li" v-for="(items,index) in header_items" :key= "index"><a href="#" class="nav_li_link">{{items.header_text}}</a></li>
-            </ul>
+        <div class="right_nav">
+            <div class="menu_nav">
+            <input type="checkbox" name="" id="nav_toggle">
             <div class="hamburger">
-                <span></span>
-                <span></span>
-                <span></span>
+            <label for="nav_toggle">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                </svg>
+            </label>
             </div>
-        </nav>
-    </header>
+            
+            <ul class="nav_ul">
+                <li class="nav_li" v-for="(items,index) in header_items" :key="index">
+                <a href="#" class="nav_a">{{items.header_text}}</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+        </div>
+    </nav>
 </template>
 
 <script type="module" src="./Header.ts">
--- src/router/index.ts
+++ src/router/index.ts
@@ -65,6 +65,14 @@ const routes: Array<RouteRecordRaw> = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Press/Press.vue')
   },
+  {
+    path: '/navbar',
+    name: 'Headernew',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Headernew.vue')
+  },
 ]
 
 const router = createRouter({
